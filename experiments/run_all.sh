348c1094080d33b53653ba7c6a13c8d8614a11d5
Semyon Bevzuk <semen.bevzuk@gmail.com>
2018-12-11 12:54:03 +0300

    Update experiments

--- experiments/run_all.sh
+++ experiments/run_all.sh
@@ -1,6 +1,6 @@
-declare -a size=(100 1000 10000 100000 500000 1000000 2000000 5000000 10000000)
+declare -a size=(100 1000 10000 100000 500000 1000000 5000000 10000000 25000000 50000000 100000000)
 left=0
-right=10
+right=1000000
 
 rm Bubble/run.csv
 rm BucketInsertion/run.csv
@@ -9,7 +9,7 @@ rm Heap/run.csv
 rm Merge/run.csv
 rm Quick/run.csv
 rm QuickLomuto/run.csv
-rm SearchTree/run.csv
+#rm SearchTree/run.csv
 rm Standart/run.csv
 
 for s in "${size[@]}"
@@ -17,10 +17,10 @@ do
 	#../bin/sample_lab -f ../experiments/Bubble/run -s Bubble -n $s -l $left -r $right
 	../bin/sample_lab -f ../experiments/BucketStandart/run -s BucketStandart -n $s -l $left -r $right -b 10000
 	../bin/sample_lab -f ../experiments/BucketInsertion/run -s BucketInsertion -n $s -l $left -r $right -b 10000
-	../bin/sample_lab -f ../experiments/Heap/run -s Heap -n $s -l $left -r $right -d 4
+	../bin/sample_lab -f ../experiments/Heap/run -s Heap -n $s -l $left -r $right -d 8
 	../bin/sample_lab -f ../experiments/Merge/run -s Merge -n $s -l $left -r $right -k 8
 	../bin/sample_lab -f ../experiments/Quick/run -s Quick -n $s -l $left -r $right
-	#../bin/sample_lab -f ../experiments/QuickLomuto/run -s QuickLomuto -n $s -l $left -r $right
-	../bin/sample_lab -f ../experiments/SearchTree/run -s SearchTree -n $s -l $left -r $right
+	../bin/sample_lab -f ../experiments/QuickLomuto/run -s QuickLomuto -n $s -l $left -r $right
+	#../bin/sample_lab -f ../experiments/SearchTree/run -s SearchTree -n $s -l $left -r $right
 	../bin/sample_lab -f ../experiments/Standart/run -s Standart -n $s -l $left -r $right
 done 
--- experiments/run_heap_dif_d.sh
+++ experiments/run_heap_dif_d.sh
@@ -1,7 +1,7 @@
-declare -a size=(100 1000 10000 100000 500000 1000000 2000000 5000000 10000000)
+declare -a size=(100 1000 10000 100000 500000 1000000 5000000  10000000 )
 declare -a dParams=(2 4 8 16 32)
 left=0
-right=10
+right=1000000
 
 rm Heap/*.csv
 for d in "${dParams[@]}"
